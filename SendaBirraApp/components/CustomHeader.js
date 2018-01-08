9df5785fb14384b87b5a303fb71c00c933b96ca4
Andres' Work <andres@reactable.com>
2018-01-08 19:49:00 +0100

    Quitar todo el contenido de Homescreen y copiamos el header del proyecto anterior

---
+++ SendaBirraApp/components/CustomHeader.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+    View,
+    Text, 
+    StyleSheet
+} from 'react-native';
+
+export class CustomHeader extends React.Component {
+    render() {
+        const {background, title, statusDark} = styles;
+        const {titleText} = this.props;
+        return (
+            <View>
+                <View style={statusDark}></View>
+                <View style={background} >
+                    <Text style = {title}> {titleText} </Text>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    statusDark:{
+        height: 24,
+        backgroundColor:'#1976D2'
+    },
+    background: {
+        paddingTop: 8,
+        flexDirection: 'column',
+        backgroundColor: '#03A9F4',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height : 80,
+        shadowColor:'#000',
+        shadowOffset : {width : 0, height :2},
+        shadowOpacity :0.4
+    },
+    title: {
+        fontSize:24,
+        color: '#FFFFFF'
+    }
+})
+
