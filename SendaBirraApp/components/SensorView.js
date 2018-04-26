349d74ccb2b8e0ecdeb9a64a1faadfa7162afce5
Andres' Personal <andres@reactable.com>
2018-04-26 17:22:22 +0200

    layout changes in sensorview

--- SendaBirraApp/components/SensorView.js
+++ SendaBirraApp/components/SensorView.js
@@ -37,7 +37,7 @@ const styles = StyleSheet.create({
     },
     infoContainer: {
         flex: 1,
-        justifyContent: "flex-start",
+        justifyContent: "space-between",
         alignItems : 'center'
     },
     warning: {
